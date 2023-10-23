59cb0fb5afe81e2adeb087ac01ee669f1a35d334
kait-codes <kktremble2.0@gmail.com>
2023-10-23 10:58:49 -0400

    created a script, added some folders, and downloaded images

---
+++ kait-script.R
@@ -0,0 +1,11 @@
+# install gapminder
+install.packages('gapminder')
+library(gapminder)
+install.packages('tidyverse')
+library(tidyverse)
+
+read_data <- gapminder
+
+dir.create('images')
+dir.create('docs')
+dir.create('figures')
