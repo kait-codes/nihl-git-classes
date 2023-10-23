2ec4bf0ca56992ba9ab1733f614607f315a67aa3
kait-codes <kktremble2.0@gmail.com>
2023-10-23 11:09:45 -0400

    Editing script with comments

--- kait-script.R
+++ kait-script.R
@@ -1,8 +1,8 @@
-# install gapminder
 install.packages('gapminder')
 library(gapminder)
 install.packages('tidyverse')
 library(tidyverse)
+#idk what the point of installing these packages was
 
 read_data <- gapminder
 
